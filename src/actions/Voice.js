91fb91646d151118055736bbc4760a525d3d8d1d
Sonahit <grandpajok@gmail.com>
2019-05-02 08:03:30 +0600

    Added playList implementation

--- src/actions/Voice.js
+++ src/actions/Voice.js
@@ -7,6 +7,11 @@ const validation = new Validation();
 const config = validation.config;
 const http = require("http");
 const { awaitRadioChoose, awaitEmbedReply } = require("../utils/Await");
+const { google } = require('googleapis');
+const youApi = google.youtube({
+  version: "v3",
+  auth: "AIzaSyAPBSJV6CrlLrpSy_3T1KJhlM_i6VpNLd8"
+});
 
 class Voice {
   constructor() {
@@ -83,7 +88,7 @@ class Voice {
       }
     } else {
       embed = validation.clearEmbed(embed);
-       if (this.data.onAir) {
+      if (this.data.onAir) {
         embed.setColor("0x004444").setDescription(`Radio is on air!`);
       } else if (this.data.playing) {
         embed.setColor("0x004444").setDescription(`I am playing a song!`);
@@ -94,23 +99,16 @@ class Voice {
 
   radio(msg) {
     if (msg.member != null) {
-      if (
-        msg.member.voice.channel &&
-        !this.data.onAir &&
-        !this.data.playing
-      ) {
+      if (msg.member.voice.channel && !this.data.onAir && !this.data.playing) {
         embed = validation.clearEmbed(embed);
         msg.member.voice.channel
           .join()
           .then(async connection => {
             embed = validation.clearEmbed(embed);
-            http.get(
-              await awaitRadioChoose(msg, msg.author, embed),
-              res => {
-                this.data.onAir = true;
-                this.data.dispatcher = connection.play(res, streamOptions);
-              }
-            );
+            http.get(await awaitRadioChoose(msg, msg.author, embed), res => {
+              this.data.onAir = true;
+              this.data.dispatcher = connection.play(res, streamOptions);
+            });
           })
           .catch(console.error);
       } else {
@@ -144,23 +142,26 @@ class Voice {
     }
   }
 
-  async stop(msg, client, mode = "force") {
-    if (msg.content === `${config.prefix}stop` || mode === "skip" || this.data.dispatcher) {
+  stop(msg, client, mode = "force") {
+    if (
+      msg.content === `${config.prefix}stop` ||
+      mode === "skip" ||
+      this.data.dispatcher
+    ) {
       if (
         msg.member.voice.channel &&
         !this.data.onAir &&
         this.data.playing === true
       ) {
         if (mode === "force") {
-          this.data.dispatcher.end("force");
-          this.data.dispatcher = false;
+          this.data.dispatcher.emit("end", "force");
           this.data.playing = false;
           msg.reply(`Stopped playing songs`);
         } else {
-          this.data.dispatcher.end("skip");
+          this.data.dispatcher.emit("end", "skip");
           msg.reply(`Skipped song`);
         }
-      } else if(this.data.onAir) {
+      } else if (this.data.onAir) {
         this.data.onAir = false;
         msg.reply(`Shutting down radio...`);
         http.globalAgent.destroy();
@@ -176,7 +177,7 @@ class Voice {
 
   volume(msg) {
     if (msg.member.voice.channel && this.data.dispatcher != false) {
-      let volume = msg.content.substring(8, msg.content.length);
+      let volume = msg.content.substring(config.prefix.length + "volume".length, msg.content.length );
       if (volume <= 200) {
         this.data.dispatcher.setVolume(parseFloat(volume / 1000));
       } else {
@@ -199,11 +200,117 @@ class Voice {
     if (this.data.skippedSong != null && this.data.skippedSong != false) {
       this.data.queue.unshift(this.data.skippedSong);
       this.data.queue.unshift(this.data.skippedSong);
-      this.data.dispatcher.end("rerun");
+      this.data.dispatcher.emit("end", "rerun");
     } else {
       msg.reply(`You didn't skip any song`);
     }
   }
+
+  async playList (msg){
+    if(!msg.content.includes(`${config.prefix}playList play`)){
+      let playListURL = msg.content.substring(config.prefix.length + "playList".length + 1, msg.content.length);
+      let playlistId = isListURL(playListURL) ? parseIntoId(playListURL) : playListURL; 
+      const Discord = require('discord.js');
+      let embed = new Discord.MessageEmbed();
+      embed.setColor("0x004444");
+      let options = {
+        part: "snippet",
+        playlistId : playlistId
+      };  
+      let videos = await getYoutubePlayList(options);
+      const setFields = () => {
+        videos.forEach(async video => {
+          await ytdlVideo.getInfo(video.snippet.resourceId.videoId).then( info => {
+            let durationMin = Math.floor(info.length_seconds / 60);
+            let durationSec = Math.ceil(info.length_seconds % 60);
+            embed.addField(
+              `Author: ${info.author.name}`,
+              `Duration: ${durationMin} mins : ${durationSec} seconds. \n Title: [${info.title}](${info.video_url})`
+            );
+            if(videos.length === embed.fields.length){
+              msg.reply(embed);
+            }
+          });
+        })
+      }
+      embed.setDescription(`Play list requested by ${msg.author.username}`);
+      msg.reply('Collecting videos... Please wait!');
+      setFields(videos);
+    } else {
+      this["playList play"](msg);
+    }
+  }
+
+  async "playList play"(msg){
+    let playListURL = msg.content.substring(config.prefix.length + "playList play".length + 1, msg.content.length);
+    let playlistId = isListURL(playListURL) ? parseIntoId(playListURL) : playListURL; 
+    const Discord = require('discord.js');
+    let embed = new Discord.MessageEmbed();
+    embed.setColor("0x004444");
+    let options = {
+      part: "snippet",
+      playlistId : playlistId
+    };  
+    let videos = await getYoutubePlayList(options);
+    const setFields = () => {
+      if (msg.member.voice.channel) {
+        videos.forEach(async video => {
+              let url = `https://www.youtube.com/watch?v=${video.snippet.resourceId.videoId}`;
+              this.data.queue.push(url);
+              embed = validation.clearEmbed(embed);
+              if(!this.data.playing){
+              this.data.playing = true;
+              msg.member.voice.channel
+                .join()
+                .then(async connection => {
+                  this.data.videoData = await ytdlVideo.getInfo(url);
+                  Play(connection, this.data, msg);
+                })
+                .catch(console.error);
+              }
+        })
+      } else {
+        embed
+          .setColor("0xff0000")
+          .setDescription("You need to join a voice channel first!");
+        msg.reply(embed);
+        throw new Error(`${msg.author.username} hasn't joined voice channel`);
+      }
+    }
+    embed.setDescription(`Added to queue.\nRequested by ${msg.author.username}`);
+    msg.reply('Collecting videos... Please wait!');
+    setFields(videos);
+  }
+}
+
+function isListURL(playListURL){
+  return /https|www|youtube|com/gi.test(playListURL);
+}
+
+function parseIntoId(playListURL = ""){
+  let url = playListURL.trim();
+  url = url.substring(url.indexOf("list=")+"list=".length, url.length);
+  const id = url.split('&')[0];
+  return id;
+}
+
+function getYoutubePlayList(options){
+  return youApi.playlistItems.list({
+    part: options.part,
+    playlistId: options.playlistId,
+    pageToken : options.pageToken ? options.pageToken : ""
+  }).then( async list => {
+    if(list.data.nextPageToken){
+      options["pageToken"] = list.data.nextPageToken;
+      let videos = await getYoutubePlayList(options);
+      videos.forEach(video => {
+        list.data.items.push(video);
+      })
+    }
+    return list.data.items;
+  }).catch(err => {
+    console.log(err);
+  });
 }
 
 async function Play(connection, data, msg) {
@@ -215,11 +322,11 @@ async function Play(connection, data, msg) {
       await ytdlVideo(data.queue[0]),
       streamOptions
     );
-    console.log('STARTED PLAYING SONG');
+    console.log("STARTED PLAYING SONG");
   } catch (err) {
     msg.reply("WRONG URL");
   }
-  data.dispatcher.on("finish", function(reason) {
+  data.dispatcher.on("end", (reason) => {
     console.log(`FINISHED PLAYING A SONG BECAUSE ${reason}`);
     finish(connection, data, reason, msg);
   });
@@ -232,9 +339,8 @@ async function finish(connection, data, reason, msg) {
   }
   if (reason === "force" || data.queue.length === 0) {
     data.playing = false;
-    data.dispatcher = false;
     msg.channel.send(`End of queue`);
-    if(reason === "force"){
+    if (reason === "force") {
       data.queue = [];
     }
     connection.disconnect();
@@ -246,7 +352,7 @@ async function finish(connection, data, reason, msg) {
 function showVideoData(msg, videoData, mode = "play") {
   let durationMin = Math.floor(videoData.length_seconds / 60);
   let durationSec = Math.ceil(videoData.length_seconds % 60);
-  let stream = (durationMin && durationSec) === 0;
+  let stream = videoData.player_response.videoDetails.isLiveContent;
   embed
     .setColor("#b92727")
     .setAuthor(
@@ -265,9 +371,9 @@ function showVideoData(msg, videoData, mode = "play") {
     .fields.push({
       name: `${stream ? "Live Stream" : "Duration"}`,
       value: `${
-        durationMin === 0
+          stream
           ? `Thanks to ${videoData.author.name}`
-          : durationMin + "min"
+          : durationMin + " min"
       }  ${durationSec === 0 ? " " : durationSec + "seconds"} `
     });
   msg.channel.send(embed);
